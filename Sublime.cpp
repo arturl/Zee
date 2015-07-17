2bc7bcbcd2f0325b9eb81afea71820965c73f738
unknown <arturl@arturl5.redmond.corp.microsoft.com>
2015-07-17 12:33:04 -0700

    Add support for Sublime Text 3

--- Sublime.cpp
+++ Sublime.cpp
@@ -1,13 +1,13 @@
 #include "stdafx.h"
 #include "Editor.h"
 
-class Sublime : public Editor
+class Sublime2 : public Editor
 {
     WCHAR szPath_[_MAX_PATH];
     bool installed_;
 
 public:
-    Sublime()
+    Sublime2()
     {
         installed_ = false;
         HKEY key;
@@ -42,9 +42,56 @@ private:
     }
 };
 
-Editor* DiscoverSublime()
+Editor* DiscoverSublime2()
 {
-    return new Sublime();
+    return new Sublime2();
 }
+
+class Sublime3 : public Editor
+{
+	WCHAR szPath_[_MAX_PATH];
+	bool installed_;
+
+public:
+	Sublime3()
+	{
+		installed_ = false;
+		HKEY key;
+		REGSAM samDesired = KEY_READ | KEY_WOW64_64KEY;
+		LPCWSTR szSublimeTextHive = L"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\Sublime Text 3_is1";
+		//"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\Sublime Text 2_is1";
+		LONG lRet = RegOpenKeyExW(HKEY_LOCAL_MACHINE, szSublimeTextHive, 0, samDesired, &key);
+		if (lRet == ERROR_SUCCESS)
+		{
+			DWORD dwType = REG_SZ;
+			DWORD dwSize = sizeof(szPath_);
+			lRet = RegQueryValueExW(key, L"InstallLocation", NULL, &dwType, (LPBYTE)szPath_, &dwSize);
+			if (lRet == ERROR_SUCCESS) installed_ = true;
+		}
+	}
+	virtual bool IsInstalled()
+	{
+		return installed_;
+	}
+private:
+	virtual void GetExecutable(LPWSTR pszExe, int exeSize)
+	{
+		auto len = wcslen(szPath_);
+		if (szPath_[len - 1] == L'\\')
+			szPath_[len - 1] = '\0';
+		swprintf_s(pszExe, exeSize, L"%s\\%s", szPath_, L"sublime_text.exe");
+	}
+
+	virtual void FormatParameters(LPWSTR pszParams, int paramSize, LPCWSTR pszFilename, int lineNumber)
+	{
+		swprintf_s(pszParams, paramSize, L"\"%s\":%d:%d", pszFilename, lineNumber, 1);
+	}
+};
+
+Editor* DiscoverSublime3()
+{
+	return new Sublime3();
+}
+
 
 
